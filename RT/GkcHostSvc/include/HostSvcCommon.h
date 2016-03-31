225403e44a88f5193fb350897806253323a02c60
Li Shunan <lishunan246@gmail.com>
2016-04-01 00:16:53 +0800

    add client and server

---
+++ RT/GkcHostSvc/include/HostSvcCommon.h
@@ -0,0 +1,16 @@
+//
+// Created by lishunan on 16-3-31.
+//
+
+#ifndef GKC_HOSTSVCCOMMON_H
+#define GKC_HOSTSVCCOMMON_H
+
+#include <iostream>
+#include <asio.hpp>
+#include <string>
+
+namespace GkcHostSvc {
+    static const int PORT_INT(10000);
+    static const std::string PORT("10000");
+}
+#endif //GKC_HOSTSVCCOMMON_H
